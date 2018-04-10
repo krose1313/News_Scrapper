104cb5793389e40f4db7eab69d43ac3aec337367
krose1313 <kathrynrose40@gmail.com>
2018-04-09 18:31:20 -0700

    newsscrapper commit 3

--- server.js
+++ server.js
@@ -68,7 +68,8 @@ app.get("/scrape", function (req, res) {
         })
         .catch(function (err) {
           // If an error occurred, send it to the client
-          return res.json(err);
+          // return res.json(err);
+          console.log("error", err);
         });
     });
 
